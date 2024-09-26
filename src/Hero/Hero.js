162bd30dafb9d53e1c29b4be0650fc0a8c5dc6d2
JanviN625 <janvinandwani@gmail.com>
2024-09-26 17:47:31 -0400

    Brought PBudget code in

---
+++ src/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <header className="hero" aria-label="Background image of a phone and laptop with charts">
+        <h1>Personal Budget</h1>
+        <h2>A personal-budget management app</h2>
+    </header>
+  );
+}
+
+export default Hero;
